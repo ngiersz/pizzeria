074abbe673d8db92d0bb9d37d262308645b7da11
Natalia Gierszewska <ngiersz@gmail.com>
2018-11-12 00:34:23 +0100

    Cleanup

--- src/main/java/com/put/bd/pizzeria/controller/ClientController.java
+++ src/main/java/com/put/bd/pizzeria/controller/ClientController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/clients")
