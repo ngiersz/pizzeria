e7453c24bcf7d3c70cbc51a88dc4f56a86473a0b
Natalia Gierszewska <ngiersz@gmail.com>
2018-12-19 19:18:00 +0100

    Add swagger

---
+++ pizzeria/src/main/java/com/put/bd/pizzeria/configuration/SwaggerResourcesConfig.java
@@ -0,0 +1,18 @@
+package com.put.bd.pizzeria.configuration;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+public class SwaggerResourcesConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
