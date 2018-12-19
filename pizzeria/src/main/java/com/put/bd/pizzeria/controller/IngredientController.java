90eeeaa93f27d63fff8e90002bf9f34358fb18a5
Natalia Gierszewska <ngiersz@gmail.com>
2018-12-19 13:54:12 +0100

    Change endpoint to /ingredients

--- pizzeria/src/main/java/com/put/bd/pizzeria/controller/IngredientController.java
+++ pizzeria/src/main/java/com/put/bd/pizzeria/controller/IngredientController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/ingredient")
+@RequestMapping(value = "/ingredients")
 public class IngredientController {
 
     @Autowired
