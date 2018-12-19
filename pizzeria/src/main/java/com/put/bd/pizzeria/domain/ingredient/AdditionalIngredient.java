1297b79e7b6dadf66e003c777e32f99628c01310
Natalia Gierszewska <ngiersz@gmail.com>
2018-12-19 17:57:28 +0100

    Add orders with additional ingredients

--- pizzeria/src/main/java/com/put/bd/pizzeria/domain/ingredient/AdditionalIngredient.java
+++ pizzeria/src/main/java/com/put/bd/pizzeria/domain/ingredient/AdditionalIngredient.java
@@ -1,38 +1,29 @@
-//package com.put.bd.pizzeria.domain.ingredient;
-//
-//import com.fasterxml.jackson.annotation.JsonIgnore;
-//import com.put.bd.pizzeria.domain.OrderedDish;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//import org.hibernate.annotations.OnDelete;
-//import org.hibernate.annotations.OnDeleteAction;
-//
-//import javax.persistence.*;
-//
-//@Entity
-//@Table(name = "additional_ingredient")
-//@Getter
-//@Setter
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class AdditionalIngredient {
-//
-//    @Id
-//    Integer id;
-//
-//    @ManyToOne
-////    @JoinColumn(name = "ordered_dish_id", columnDefinition = "INT")
-////    OrderedDish orderedDish;
-//
-////    @Column(name = "ingredient_id", columnDefinition = "INT")
-////    Integer ingredientId;
-//
-//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-//    @JoinColumn(name = "ingredient_id", nullable = false)
-//    @OnDelete(action = OnDeleteAction.CASCADE)
-//    @JsonIgnore
-//    Ingredient ingredient;
-//
-//}
+package com.put.bd.pizzeria.domain.ingredient;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "additional_ingredient")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdditionalIngredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Integer id;
+
+    @Column(name = "ordered_dish_id", columnDefinition = "INT")
+    Integer orderedDishId;
+
+    @Column(name = "ingredient_id", columnDefinition = "INT")
+    Integer ingredientId;
+
+}
--- pizzeria/src/main/java/com/put/bd/pizzeria/service/OrderService.java
+++ pizzeria/src/main/java/com/put/bd/pizzeria/service/OrderService.java
@@ -1,19 +1,21 @@
 package com.put.bd.pizzeria.service;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.put.bd.pizzeria.domain.Client;
-import com.put.bd.pizzeria.domain.DishMenu;
 import com.put.bd.pizzeria.domain.Order;
 import com.put.bd.pizzeria.domain.OrderedDish;
+import com.put.bd.pizzeria.domain.ingredient.AdditionalIngredient;
+import com.put.bd.pizzeria.domain.ingredient.Ingredient;
 import com.put.bd.pizzeria.persistance.OrderRepository;
 import com.put.bd.pizzeria.persistance.OrderedDishRepository;
+import com.put.bd.pizzeria.persistance.ingredient.AdditionalIngredientRepository;
+import com.put.bd.pizzeria.persistance.ingredient.IngredientRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.sql.SQLException;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -26,6 +28,12 @@ public class OrderService {
 
     @Autowired
     OrderedDishRepository orderedDishRepository;
+
+    @Autowired
+    AdditionalIngredientRepository  additionalIngredientRepository;
+
+    @Autowired
+    IngredientRepository ingredientRepository;
 
     @Autowired
     ClientService clientService;
@@ -63,8 +71,20 @@ public class OrderService {
             System.out.println("wysłane przez klienta: " + dish.getId() + " " + dish.getDishMenu() + " " + dish.getOrder() + " " + dish.getAdditionalIngredients());
             OrderedDish orderedDish = new OrderedDish(get(orderId), dish);
             System.out.println("dodawane do bazy danych: " + orderedDish.getId() + " " + orderedDish.getDishMenu() + " " + orderedDish.getOrder() + " " + orderedDish.getAdditionalIngredients());
+            Set<Ingredient> createdAdditionalIngredients = saveAdditionalIngredients(orderedDish.getId(), orderedDish.getAdditionalIngredients());
+            orderedDish.setAdditionalIngredients(createdAdditionalIngredients);
             orderedDishRepository.save(orderedDish);
         }
+    }
+
+    private Set<Ingredient> saveAdditionalIngredients(Integer orderedDishId, Set<Ingredient> ingredients) {
+        Set<Ingredient> createdIngredients = new HashSet<>();
+        for (Ingredient ingredient : ingredients) {
+            AdditionalIngredient additionalIngredient = new AdditionalIngredient(null, orderedDishId, ingredient.getId());
+            additionalIngredientRepository.save(additionalIngredient);
+            createdIngredients.add(ingredientRepository.findById(additionalIngredient.getIngredientId()).get());
+        }
+        return createdIngredients;
     }
 
     private Integer createNewOrder(Client client){
