7ec1fedd789e290b99ba7f430f056eed105ad549
Natalia Gierszewska <ngiersz@gmail.com>
2019-01-08 15:57:16 +0100

    Change LogsController

---
+++ pizzeria/src/main/java/com/put/bd/pizzeria/controller/LogsController.java
@@ -0,0 +1,34 @@
+package com.put.bd.pizzeria.controller;
+
+import com.put.bd.logging.Log;
+import com.put.bd.logging.LoggingRepository;
+import com.put.bd.pizzeria.domain.OrderSubmission;
+import com.put.bd.pizzeria.persistance.OrderSubmissionRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class LogsController {
+
+    @Autowired
+    OrderSubmissionRepository orderSubmissionRepository;
+
+    @Autowired
+    LoggingRepository loggingRepository;
+
+    @RequestMapping(value = "/ordersubmissions", method = RequestMethod.GET)
+    public List<OrderSubmission> getAllOrderSubmissiosn() {
+        return orderSubmissionRepository.findAll();
+    }
+
+    @RequestMapping(value = "/logs", method = RequestMethod.GET)
+    public List<Log> getAllLogs() {
+        return loggingRepository.findAll();
+    }
+
+}
+
